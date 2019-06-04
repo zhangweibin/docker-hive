ffaed51a805c747c8ec0432d501b488e13fa6dc3
perf2 <root@perf2.com>
2019-06-04 19:58:41 +0800

    fix startup script and configuration

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ hadoop fs -chmod g+w   /tmp
 hadoop fs -chmod g+w   /user/hive/warehouse
 
 hadoop fs -mkdir -p    /apps/tez
-hadoop fs -put         /opt/tez/share/tez.tar.gz /apps/tez
+hadoop fs -put         /opt/tez/share/tez.tar.gz /apps/tez/tez.tar.gz
 
 cd $HIVE_HOME/bin
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
